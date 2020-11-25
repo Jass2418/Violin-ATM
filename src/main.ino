c2554963bf84b0a9a857456d579c6d6603a93969
Jesus Salazar <a.salazar2418@gmail.com>
2020-11-24 19:51:44 -0600

    Primer Commit Tesis UANL Prostodoncia

---
+++ src/main.ino
@@ -0,0 +1,17 @@
+//
+// Created by Jesus Salazar on 11/24/20.
+//
+
+#define STRAIN_GAUGE A0
+#define LEFT_MUSCLE A1
+#define RIGHT_MUSCLE A2
+
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
+void setup(){
+}
+
+void loop(){
+
+}
